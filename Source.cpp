dcb81972a80c9d6c4ec85822ae5c695c42d63963
diashka01 <90564818+diashka01@users.noreply.github.com>
2021-11-01 18:31:41 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int vvod()
+{
+	double chislo;
+	cin >> chislo;
+	if (chislo < 0 || chislo > 100000 || (chislo - int(chislo) != 0) || cin.fail())
+	{
+		cout << "Ошибка" << endl;
+		cin.clear();
+	}
+	else
+		return chislo;
+
+}
+int factorial(int vvod)
+{
+	long long int fact;
+	int i;
+	fact = 1;
+	for (i = 1; i <= vvod; i++)
+		fact = fact * i;
+	return fact;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int vvodim;
+	long long int vivodim;
+	cout << "Введите число: ";
+	vvodim = vvod();
+	vivodim = factorial(vvodim);
+	cout << "Факториал данного числа: " << vivodim;
+
+
+}
