04faabb28f0e438e2aea77a33715765a4aad495d
diashka01 <90564818+diashka01@users.noreply.github.com>
2021-12-27 02:35:38 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+class LIFO
+{
+private:
+	int* p, len, elem;
+public:
+	LIFO(int n)
+	{
+		len = n;
+		p = new int[len];
+		elem = 0;
+	}
+	~LIFO()
+	{
+		delete[] p;
+	}
+
+	void push(int pl)
+	{
+		p[elem] = pl;
+		elem++;
+	}
+	int pop() {
+		int	i = 0;
+		cout << "извлечен элемент " << p[len-1] << "\n";
+		if (len == 0)
+			return 0;
+		len--;
+		return p[len];
+	}
+	void print()
+	{
+		for (int i = 0; i < len; i++) {
+			cout << p[i];
+			if (i < len - 1)
+				cout << " ";
+		}
+		cout << endl;
+	}
+};
+
+int main()
+{
+	int n, i, elem;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите количество элементов ";
+	cin >> n;
+	LIFO massiv(n);
+	i = 0;
+	cout << "Введите элементы ";
+	while (i < n)
+	{
+		cin >> elem;
+		massiv.push(elem);
+		i++;
+	}
+	massiv.pop();
+	massiv.print();
+}
